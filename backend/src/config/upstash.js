aaa8f42382bb4c398bf8d196d6c9a52b412a8f22
Marius <mariussangwa@gmail.com>
2026-03-05 06:42:12 +0200

    increased the ratelimit seconds per minute

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const ratelimit = new Ratelimit({
     redis:Redis.fromEnv(),
-    limiter:Ratelimit.slidingWindow(10,"20 s")
+    limiter:Ratelimit.slidingWindow(100,"60 s")
 });
 
 module.exports = ratelimit;
